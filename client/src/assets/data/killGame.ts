68cbb16bafed928102d1fdf2a09cd60f3d77f2e4
hubert <hubert.gorski@bowwe.com>
2024-05-09 01:59:01 +0200

    admin tables in kill game

--- client/src/assets/data/killGame.ts
+++ client/src/assets/data/killGame.ts
@@ -1,13 +1,65 @@
-import { KILL_GAME_CARD_STATUS, KillGameCard } from "@/mod/killGame/models/KillGameCard"
-import { KILL_GAME_USER_STATUS, KillGameUser } from "@/mod/killGame/models/KillGameUser"
+import {
+  KILL_GAME_CARD_STATUS,
+  KillGameCard,
+} from "@/mod/killGame/models/KillGameCard";
+import {
+  KILL_GAME_USER_STATUS,
+  KillGameUser,
+} from "@/mod/killGame/models/KillGameUser";
 
 export const killGameData = {
-    user: new KillGameUser(1, 4, 2, KILL_GAME_USER_STATUS.UNCERTAIN, 2),
-    usersAlive: 17,
-    cards: [
-        new KillGameCard(1,1,"Oskar Litka",'Spirit', 'Zrób słowiański przykuc', KILL_GAME_CARD_STATUS.EXPECTANCY),
-        new KillGameCard(2,1,"Amadeusz Hyży",'Szkolenie', 'Zaśpiewaj coś z disneya', KILL_GAME_CARD_STATUS.INACTIVE, 1),
-        new KillGameCard(2,1,"Stanisław Kot",'Ogarniam', 'Poprowadź grupową grę dla conajmniej 10 uczestników', KILL_GAME_CARD_STATUS.ACTIVE),
-        new KillGameCard(3,1,"Klara Orłowska",'Ognisty krąg', 'Zrób tosta', KILL_GAME_CARD_STATUS.INACTIVE, 2, 'Laura Chrzanowska')
-    ]
-}
+  user: new KillGameUser(1, 4, 2, KILL_GAME_USER_STATUS.UNCERTAIN, 2),
+  usersAlive: 17,
+  cards: [
+    new KillGameCard(
+      1,
+      1,
+      "Oskar Litka",
+      "Spirit",
+      "Zrób słowiański przykuc",
+      KILL_GAME_CARD_STATUS.EXPECTANCY
+    ),
+    new KillGameCard(
+      2,
+      1,
+      "Amadeusz Hyży",
+      "Szkolenie",
+      "Zaśpiewaj coś z disneya",
+      KILL_GAME_CARD_STATUS.INACTIVE,
+      1
+    ),
+    new KillGameCard(
+      2,
+      1,
+      "Stanisław Kot",
+      "Ogarniam",
+      "Poprowadź grupową grę dla conajmniej 10 uczestników",
+      KILL_GAME_CARD_STATUS.ACTIVE
+    ),
+    new KillGameCard(
+      3,
+      1,
+      "Klara Orłowska",
+      "Ognisty krąg",
+      "Zrób tosta",
+      KILL_GAME_CARD_STATUS.INACTIVE,
+      2,
+      "Laura Chrzanowska"
+    ),
+  ],
+};
+
+export const killersData = [
+  { killerId: 1, killerName: "Adam", cardsOwned: 12, killingsCommitted: 3 },
+  { killerId: 2, killerName: "Laura", cardsOwned: 11, killingsCommitted: 6 },
+  { killerId: 3, killerName: "Oskar", cardsOwned: 7, killingsCommitted: 7 },
+  { killerId: 4, killerName: "Kuba", cardsOwned: 10, killingsCommitted: 2 },
+  { killerId: 5, killerName: "Magda", cardsOwned: 5, killingsCommitted: 5 },
+  { killerId: 6, killerName: "Igor", cardsOwned: 32, killingsCommitted: 13 },
+];
+
+export const cardsData = [
+  { killerId: 1, killerName: "Adam", userId: 2, userName: "Hubert", action: 'zjedz tosty', word: 'spirit'},
+  { killerId: 3, killerName: "Laura", userId: 4, userName: "Kacper", action: 'zrób słowiański przykuc', word: 'ognisty krąg'},
+  { killerId: 5, killerName: "Grzesiu", userId: 6, userName: "Oskar", action: 'polej alkohol', word: 'zdrowie'},
+];
--- client/src/mod/killGame/admin/AdminKillGameView.vue
+++ client/src/mod/killGame/admin/AdminKillGameView.vue
@@ -2,12 +2,31 @@
 import { ref } from "vue";
 import KillGameAdminNotificationsPanel from "./KillGameAdminNotificationsPanel.vue";
 import { notifications } from "@/assets/data/notifications";
+import { killersData, cardsData } from "@/assets/data/killGame";
 
 const tabs = [
   { id: 0, title: "killGame.killCards" },
   { id: 1, title: "killGame.gamers" },
 ];
 const selectedTab = ref<number>(tabs[0].id);
+
+const headersKillersTable = ref([
+  { title: "Imię", key: "killerName", sortable: true },
+  { title: "Karty", key: "cardsOwned", align: "center", sortable: true },
+  {
+    title: "Zabójstwa",
+    key: "killingsCommitted",
+    align: "center",
+    sortable: true,
+  },
+]);
+
+const headersCardsTable = ref([
+  { title: "Zabójca", key: "killerName" },
+  { title: "Ofiara", key: "userName" },
+  { title: "Akcja", key: "action" },
+  { title: "Hasło", key: "word" },
+]);
 
 </script>
 
@@ -20,12 +39,31 @@ const selectedTab = ref<number>(tabs[0].id);
     </v-tabs>
     <v-window v-model="selectedTab">
       <v-window-item>
-        {{ selectedTab }}
+        <v-data-table
+          :headers="headersCardsTable"
+          :items="cardsData"
+          class="mobile-table"
+        >
+        </v-data-table>
       </v-window-item>
       <v-window-item>
-        {{ selectedTab }}
+        <v-data-table
+          :headers="headersKillersTable"
+          :items="killersData"
+          class="mobile-table"
+        >
+        </v-data-table>
       </v-window-item>
     </v-window>
     <kill-game-admin-notifications-panel :notifications="notifications" />
   </div>
 </template>
+
+<style scoped>
+.mobile-table .v-data-table__wrapper {
+  overflow-x: auto;
+}
+.mobile-table .v-data-table__wrapper table {
+  width: auto !important;
+}
+</style>
