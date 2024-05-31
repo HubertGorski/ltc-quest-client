96e688b8e98ce50414acec77c5031d1bb58fb211
hubert <hubert.gorski@bowwe.com>
2024-05-31 21:30:08 +0200

    color tabs titles

--- client/src/mod/killGame/admin/AdminKillGameAddCardsView.vue
+++ client/src/mod/killGame/admin/AdminKillGameAddCardsView.vue
@@ -1,22 +1,34 @@
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, watch } from "vue";
 import { ref } from "vue";
 import { actionsKeyData } from "@/assets/data/killGame";
 import { wordsKeyData } from "@/assets/data/killGame";
 import { killersData } from "@/assets/data/killGame";
 import type { KillGameActionKey } from "../models/KillGameActionKey";
 import type { KillGameWordKey } from "../models/KillGameWordKey";
+import { noSelectedTeam } from "@/models/Team";
 
 const minimumRequiredPlayers: number = 2;
 
 const actionsKey = ref<KillGameActionKey[]>(actionsKeyData);
 const wordsKey = ref<KillGameWordKey[]>(wordsKeyData);
+const panel = ref(null);
+
+const isSelectedAllUsers = ref<boolean>(true);
 
 const selectedActions = ref<KillGameActionKey[]>([]);
 const selectedWords = ref<KillGameWordKey[]>([]);
 const selectedPlayers = ref<number[]>(
   killersData.map((killer) => killer.userId)
 );
+
+watch(isSelectedAllUsers, (newIsSelectedAllUsers) => {
+  if (newIsSelectedAllUsers) {
+    selectedPlayers.value = killersData.map((killer) => killer.userId);
+  } else {
+    selectedPlayers.value = [];
+  }
+});
 
 const isActionsListComplete = computed((): boolean => {
   return selectedActions.value.length >= selectedPlayers.value.length;
@@ -27,7 +39,15 @@ const isWordsListComplete = computed((): boolean => {
 const isPlayersListComplete = computed((): boolean => {
   return selectedPlayers.value.length > minimumRequiredPlayers;
 });
-
+const isExpandedTab0 = computed((): boolean => {
+  return panel.value !== null ? panel.value === 0 : false;
+});
+const isExpandedTab1 = computed((): boolean => {
+  return panel.value ? panel.value === 1 : false;
+});
+const isExpandedTab2 = computed((): boolean => {
+  return panel.value ? panel.value === 2 : false;
+});
 const isGenerateBtnAvailable = computed((): boolean => {
   return (
     isPlayersListComplete.value &&
@@ -35,12 +55,18 @@ const isGenerateBtnAvailable = computed((): boolean => {
     isWordsListComplete.value
   );
 });
+
+const addUserIdToArray = (id: number) => {
+  console.log(id);
+};
 </script>
 
 <template>
-  <v-expansion-panels class="killGameAddCards">
+  <v-expansion-panels class="killGameAddCards" v-model="panel">
     <v-expansion-panel>
-      <v-expansion-panel-title>
+      <v-expansion-panel-title
+        :class="[isExpandedTab0 ? `bg-${noSelectedTeam.color}` : '']"
+      >
         <span
           >{{ $t("killGame.selectPlayers") }} ({{
             selectedPlayers.length
@@ -59,18 +85,47 @@ const isGenerateBtnAvailable = computed((): boolean => {
           ></v-icon></template
       ></v-expansion-panel-title>
       <v-expansion-panel-text>
-        <div v-for="killer in killersData" :key="killer.userId">
-          <v-checkbox
-            v-model="selectedPlayers"
-            :label="killer.userName"
-            :value="killer.userId"
-            hide-details
-          ></v-checkbox>
+        <v-text-field
+          class="pt-2"
+          append-inner-icon="mdi-magnify"
+          density="compact"
+          label="Wyszukaj użytkownika"
+          variant="solo"
+          hide-details
+          single-line
+        ></v-text-field>
+        <v-list>
+          <v-list-item
+            v-for="killer in killersData"
+            :key="killer.userId"
+            @click="addUserIdToArray(killer.userId)"
+          >
+            <v-list-item-title class="pl-2">{{
+              killer.userName
+            }}</v-list-item-title>
+            <v-divider></v-divider>
+          </v-list-item>
+        </v-list>
+        <div class="">
+          <v-btn>
+            <span class="pr-2">Zaznacz</span>
+            <v-icon>mdi-expand-all-outline</v-icon>
+          </v-btn>
+          <v-btn>
+            <span class="pr-2">Odznacz</span>
+            <v-icon>mdi-collapse-all-outline</v-icon>
+          </v-btn>
+          <v-btn>
+            <span class="pr-2">Wylosuj</span>
+            <v-icon>mdi-dice-multiple-outline</v-icon>
+          </v-btn>
         </div>
       </v-expansion-panel-text>
     </v-expansion-panel>
     <v-expansion-panel>
-      <v-expansion-panel-title>
+      <v-expansion-panel-title
+        :class="[isExpandedTab1 ? `bg-${noSelectedTeam.color}` : '']"
+      >
         <span
           >{{ $t("killGame.selectActions") }} ({{ selectedActions.length }}/{{
             selectedPlayers.length
@@ -100,7 +155,9 @@ const isGenerateBtnAvailable = computed((): boolean => {
       </v-expansion-panel-text>
     </v-expansion-panel>
     <v-expansion-panel>
-      <v-expansion-panel-title>
+      <v-expansion-panel-title
+        :class="[isExpandedTab2 ? `bg-${noSelectedTeam.color}` : '']"
+      >
         <span
           >{{ $t("killGame.selectWords") }} ({{ selectedWords.length }}/{{
             selectedPlayers.length
